a5d436bf66dc6b77019c20d14d264e4a7492f6ea
NITHIN <nithinsiva144@gmail.com>
2026-08-10 15:00:06 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (30.05%) - LeetSync

---
+++ 223-rectangle-area/rectangle-area.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+       int A1 = (ax2 - ax1) * (ay2 - ay1);
+        int A2 = (bx2 - bx1) * (by2 - by1);
+        int w = Math.max(0 ,Math.min(ax2, bx2) - Math.max(ax1, bx1));
+        int h = Math.max(0,Math.min(ay2, by2) - Math.max(ay1, by1));
+        int o=w*h;
+        int A=A1+A2-o;
+        return A;
+    }
+}
