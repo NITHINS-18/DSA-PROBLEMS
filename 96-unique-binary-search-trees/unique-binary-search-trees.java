b00d97ea9e11a522eae4e3e70f0fe0bb8f7efa71
NITHIN <nithinsiva144@gmail.com>
2026-08-11 14:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (69.65%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -5,9 +5,7 @@ class Solution {
         arr[1] = 1;
         for (int i=2;i<=n;i++){
             for (int j=1;j<=i;j++) {
-                int left=j-1;
-                int right =i-j;
-                arr[i]+= arr[left]*arr[right];
+                arr[i]+= arr[j-1]*arr[i-j];
             }
         }
         return arr[n];
