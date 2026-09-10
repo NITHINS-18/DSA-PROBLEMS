a1ad270415612b0b7308b365279a98bc2e850275
NITHIN <nithinsiva144@gmail.com>
2026-09-10 10:39:42 +0530

    Time: 3 ms (34.20%) | Memory: 49.2 MB (11.41%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,22 @@
+class Solution {
+    void solve(int index,String cs,List<String>ans,String digits,String[]map){
+        if(index==digits.length()){
+            ans.add(cs);
+            return;
+        }
+        String letters=map[digits.charAt(index)-'0'];
+        for(int i=0;i<letters.length();i++){
+            //  cs+=letters.charAt(i);
+              solve(index+1,cs+letters.charAt(i),ans,digits,map);
+              //cs=cs.substring(0,cs.length()-1);
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+        String[]map={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String>ans=new ArrayList<>();
+        String cs="";
+        int size=0;
+        solve(size,cs,ans,digits,map);
+        return ans;
+    }
+}
