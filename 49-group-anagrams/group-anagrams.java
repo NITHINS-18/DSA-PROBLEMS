a6aadb349d8776a84ae0d021d055f14084891353
NITHIN <nithinsiva144@gmail.com>
2026-09-11 11:27:35 +0530

    Time: 6 ms (99.06%) | Memory: 49.8 MB (33.46%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>>map=new HashMap<>();
+        for(String x : strs){
+            char[]arr=x.toCharArray();
+            Arrays.sort(arr);
+            String key=new String(arr);
+            if(!map.containsKey(key))
+            {
+                map.put(key,new ArrayList<>());
+            }
+             map.get(key).add(x);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
